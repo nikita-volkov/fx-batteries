b86d10ed227f4d72ad508e04b1b05a5a8204db58
Nikita Volkov <nikita.y.volkov@mail.ru>
2019-10-10 12:14:49 +0300

    Implement IORef

---
+++ library/FxBatteries/IORef.hs
@@ -0,0 +1,50 @@
+module FxBatteries.IORef
+where
+
+import FxBatteries.Prelude
+import Fx
+
+
+{-|
+Wrapper around `newIORef`.
+-}
+new :: a -> Provider err (IORef a)
+new a = runFx (runTotalIO (newIORef a))
+
+{-|
+Wrapper around `readIORef`.
+-}
+read :: Fx (IORef a) err a
+read = handleEnv (runTotalIO . readIORef)
+
+{-|
+Wrapper around `writeIORef`.
+-}
+write :: a -> Fx (IORef a) err ()
+write a = handleEnv (\ ref -> runTotalIO (writeIORef ref a))
+
+{-|
+Wrapper around `modifyIORef'`.
+
+__Notice:__
+To protect from a common pitfall of piling up thunks,
+this function is strict and no lazy version is provided.
+-}
+modify :: (a -> a) -> Fx (IORef a) err ()
+modify fn = handleEnv (\ ref -> runTotalIO (modifyIORef' ref fn))
+
+{-|
+Wrapper around `atomicModifyIORef'`.
+
+__Notice:__
+To protect from a common pitfall of piling up thunks,
+this function is strict and no lazy version is provided.
+-}
+interactAtomically :: (a -> (b, a)) -> Fx (IORef a) err b
+interactAtomically fn = handleEnv (\ ref -> runTotalIO (atomicModifyIORef' ref (swap . fn)))
+
+{-|
+Adaptation of `interactAtomically` to the `State` monad.
+-}
+runStateAtomically :: State a b -> Fx (IORef a) err b
+runStateAtomically = interactAtomically . runState
