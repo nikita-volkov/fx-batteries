8674148d4b9fdeb472dffa66ef982ccd7120a4cb
Nikita Volkov <nikita.y.volkov@mail.ru>
2020-03-22 02:09:00 +0300

    Add MVar

---
+++ library/FxBatteries/MVar.hs
@@ -0,0 +1,56 @@
+module FxBatteries.MVar
+where
+
+import FxBatteries.Prelude as Prelude hiding (take, put, read, modify)
+import Fx
+
+
+{-|
+Analogue to `newMVar`.
+-}
+new :: a -> Provider err (MVar a)
+new a = runFx (runTotalIO (newMVar a))
+
+{-|
+Analogue to `takeMVar`.
+-}
+take :: Fx (MVar a) err a
+take = handleEnv (runTotalIO . takeMVar)
+
+{-|
+Analogue to `putMVar`.
+-}
+put :: a -> Fx (MVar a) err ()
+put !a = handleEnv (\ var -> runTotalIO (putMVar var a))
+
+{-|
+Analogue to `readMVar`.
+-}
+read :: Fx (MVar a) err a
+read = do
+  a <- take
+  put a
+  return a
+
+{-|
+Analogue to `modifyMVar_`.
+-}
+modify :: (a -> a) -> Fx (MVar a) err ()
+modify fn = take >>= put . fn
+
+{-|
+Analogue to `modifyMVar`.
+-}
+runState :: State a b -> Fx (MVar a) err b
+runState state = do
+  a <- take
+  case Prelude.runState state a of
+    (b, a') -> do
+      put a'
+      return b
+
+{-|
+Helper executing the reader monad.
+-}
+runReader :: Reader a b -> Fx (MVar a) err b
+runReader reader = read & fmap (Prelude.runReader reader)
